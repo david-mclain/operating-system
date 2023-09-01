7134826eef654afb81fb2f4f90ebd1af6cf32aa0
david-mclain <davidmclain98@gmail.com>
2023-09-01 11:57:04 -0700

    Initial commit

---
+++ phase1a/phase1.c
@@ -0,0 +1,33 @@
+#include "phase1.h"
+
+void phase1_init(void) {
+
+}
+
+int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority) {
+    return 0;
+}
+
+int join(int *status) {
+    return 0;
+}
+
+void quit(int status, int switchToPid) {
+    while(1){}
+}
+
+int getpid(void) {
+    return 0;
+}
+
+void dumpProcesses(void) {
+
+}
+
+void startProcesses(void) {
+
+}
+
+void TEMP_switchTo(int pid) {
+
+}
