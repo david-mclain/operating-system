8a4c6487faa511e91700864ae6627ddc27683a38
david-mclain <davidmclain98@gmail.com>
2023-09-06 16:37:25 -0700

    some progress made with proc table and PCB

--- phase1a/phase1.c
+++ phase1a/phase1.c
@@ -1,10 +1,49 @@
 #include "phase1.h"
+#include <stddef.h>
+#include <string.h>
+
+// GOOGLE MEMSET FOR WIPING BIG DATA STRUCTURE
+// NEED A USLOSSSCONTEXT STRUCT, STORES STATE OF PROCESS
+
+typedef struct PCB {
+    int pid;
+    int runState;  // WHY DID I PUT RUN STATE???
+    int priority;
+
+    char processName[MAXNAME];
+
+    USLOSS_Context context;
+} PCB;
+
+PCB processes[MAXPROC];
+
+int currentPID = 1;
+
+// ContextSwitch(old, new);
+// old is what it was, new is what it currently is
+// WHEN SWITCHING CONTEXTS MAKE SURE TO SAVE STATE
 
 void phase1_init(void) {
-
+    PCB init;
+    init.pid = currentPID;
+    init.priority = 6;
+    strcpy(init.processName, "init");
+    processes[currentPID++] = init;
 }
 
-int fork1(char *name, int(*func)(char *), char *arg, int stacksize, int priority) {
+int fork1(char *name, int (*func)(char*), char *arg, int stacksize, int priority) {
+    if (stacksize < USLOSS_MIN_STACK) {
+        return -2;
+    }
+    if (priority < 1 || priority > 7) {
+        return -1;
+    }
+    if (func == NULL || arg == NULL) {
+        return -1;
+    }
+    if (strlen(arg) > MAXNAME) {
+        return -1;
+    }
     return 0;
 }
 
