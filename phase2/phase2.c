5482b97bc28109980c333054ed559e66af680264
miles <miles.gendreau@gmail.com>
2023-09-29 13:25:19 -0700

    compling!

--- phase2/phase2.c
+++ phase2/phase2.c
@@ -1,9 +1,5 @@
 #include "phase2.h"
 
-void phase2_init(void) {
-
-}
-
 int MboxCreate(int slots, int slot_size) {
     return 0;
 }
@@ -29,11 +25,30 @@ int MboxCondRecv(int mbox_id, void *msg_ptr, int msg_max_size) {
 }
 
 void waitDevice(int type, int unit, int *status) {
-
+    
 }
 
 void wakeupByDevice(int type, int unit, int status) {
+    
+}
+
+void phase2_init(void) {
+    
+}
+
+void phase2_start_service_processes() {
 
 }
+
+int phase2_check_io() {
+    return 0;
+}
+
+void phase2_clockHandler() {
+
+}
+
+
+
 
 void (*systemCallVec[])(USLOSS_Sysargs *args);
