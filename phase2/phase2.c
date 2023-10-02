f82ba70c04b19501c0e6203107c18116886175be
david-mclain <davidmclain98@gmail.com>
2023-10-02 13:08:21 -0700

    .

--- phase2/phase2.c
+++ phase2/phase2.c
@@ -4,10 +4,34 @@
 #include <string.h>
 #include <usloss.h>
 
+#define CLOCK_INDEX 0
+#define TERM_INDEX USLOSS_CLOCK_UNITS
+#define DISK_INDEX TERM_INDEX + USLOSS_CLOCK_UNITS
+
+typedef struct PCB {
+    int pid;    
+} PCB;
+
+typedef struct Message {
+    char isUsed;
+    char message[MAX_MESSAGE];
+    struct Message* nextSlot;
+    struct Message* prevSlot;
+} Message;
+
 typedef struct Mailbox {
     int id;
+    int slots;
+    int slotSize;
+
     char isReleased;
+
+    Message* firstSlot;
 } Mailbox;
+
+Message messageSlots[MAXSLOTS];
+
+int curMailboxID = 0;
 
 static Mailbox mailboxes[MAXMBOX];
 
@@ -17,10 +41,13 @@ void nullsys(void);
 
 void phase2_init(void) {
     memset(mailboxes, 0, sizeof(mailboxes));
+    memset(messageSlots, 0, sizeof(messageSlots));
     for (int i = 0; i < MAXSYSCALLS; i++) {
         systemCallVec[i] = &nullsys;
     }
-    //USLOSS_IntVec[USLOSS_SYSCALL_INT] = 
+    for (int i = 0; i < USLOSS_CLOCK_UNITS + USLOSS_DISK_UNITS + USLOSS_TERM_UNITS; i++) {
+        MboxCreate(1, sizeof(int));
+    }
 }
 
 void phase2_start_service_processes() {
@@ -31,7 +58,30 @@ int MboxCreate(int slots, int slot_size) {
     if (slots < 0 || slot_size < 0 || slot_size > MAXSLOTS) {
         return -1;
     }
-    return 0;
+    Mailbox* cur = &mailboxes[curMailboxID];
+    cur->id = curMailboxID++;
+    cur->slots = slots;
+    cur->slotSize = slot_size;
+    for (int i = 0; i < slots; i++) {
+        int j = i;
+        Message* slot = &messageSlots[j];
+        while (slot->isUsed) {
+            slot = &messageSlots[++j];
+            if (j >= MAXSLOTS) {
+                printf("erororroror\n");
+                return -1;
+                //USLOSS_Console("errororororo\n");
+                //USLOSS_Halt(1);
+            }
+        }
+        slot->nextSlot = cur->firstSlot;
+        if (cur->firstSlot) {
+            cur->firstSlot->prevSlot = slot;
+        }
+        cur->firstSlot = slot;
+        slot->isUsed = 1;
+    }
+    return cur->id;
 }
 
 int MboxRelease(int mbox_id) {
