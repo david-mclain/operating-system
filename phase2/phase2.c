4119353f53817bf401f5a01c08f8f8e10cbe11b6
david-mclain <davidmclain98@gmail.com>
2023-10-01 22:09:37 -0700

    .

--- phase2/phase2.c
+++ phase2/phase2.c
@@ -1,6 +1,36 @@
-#include "phase2.h"
+#include <phase1.h>
+#include <phase2.h>
+#include <stdio.h>
+#include <string.h>
+#include <usloss.h>
+
+typedef struct Mailbox {
+    int id;
+    char isReleased;
+} Mailbox;
+
+static Mailbox mailboxes[MAXMBOX];
+
+void (*systemCallVec[MAXSYSCALLS])(USLOSS_Sysargs *args);
+
+void nullsys(void);
+
+void phase2_init(void) {
+    memset(mailboxes, 0, sizeof(mailboxes));
+    for (int i = 0; i < MAXSYSCALLS; i++) {
+        systemCallVec[i] = &nullsys;
+    }
+    //USLOSS_IntVec[USLOSS_SYSCALL_INT] = 
+}
+
+void phase2_start_service_processes() {
+
+}
 
 int MboxCreate(int slots, int slot_size) {
+    if (slots < 0 || slot_size < 0 || slot_size > MAXSLOTS) {
+        return -1;
+    }
     return 0;
 }
 
@@ -32,14 +62,6 @@ void wakeupByDevice(int type, int unit, int status) {
     
 }
 
-void phase2_init(void) {
-    
-}
-
-void phase2_start_service_processes() {
-
-}
-
 int phase2_check_io() {
     return 0;
 }
@@ -48,7 +70,7 @@ void phase2_clockHandler() {
 
 }
 
+void nullsys() {
+    USLOSS_Console("nullsys(): Program called an unimplemented syscall.  syscall no: %d   PSR: 0x%X", 0, USLOSS_PsrGet()); //fill with proper vals later 
+}
 
-
-
-void (*systemCallVec[])(USLOSS_Sysargs *args);
