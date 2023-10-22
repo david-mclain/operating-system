33229e353c4eca0bdfa8073537ab62c4df4b83a1
david-mclain <davidmclain98@gmail.com>
2023-10-22 09:27:05 -0700

    phase4 files added, compiles

---
+++ phase4b/phase4.c
@@ -0,0 +1,31 @@
+void phase4_init(void) {
+
+}
+
+void phase4_start_service_processes() {
+
+}
+
+int kernSleep(int seconds) {
+
+}
+
+int kernDiskRead (void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+
+}
+
+int kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+
+}
+
+int kernDiskSize (int unit, int *sector, int *track, int *disk) {
+
+}
+
+int kernTermRead (char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+
+}
+
+int kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+
+}
